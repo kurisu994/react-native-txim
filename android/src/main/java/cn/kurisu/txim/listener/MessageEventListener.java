6f9df11449878865ad0ba66a26dc0f3829f25c8d
kurisu <makise_kurisuu@outlook.jp>
2020-04-17 18:02:17 +0800

    bug fix
    Signed-off-by: kurisu <makise_kurisuu@outlook.jp>

--- android/src/main/java/cn/kurisu/txim/listener/MessageEventListener.java
+++ android/src/main/java/cn/kurisu/txim/listener/MessageEventListener.java
@@ -40,8 +40,9 @@ public class MessageEventListener extends BaseListener implements TIMMessageList
         WritableArray writableArray = messageAnalysis(messageInfos);
         module.sendEvent(IMEventNameConstant.ON_NEW_MESSAGE, writableArray);
         int type = info.getMsgType();
-        if (MessageInfo.MSG_TYPE_CUSTOM == type) {
-            PushUtil.getInstance().PushNotify(info.getData(), info.getDesc());
+        String data = info.getData() == null ? "" : info.getData();
+        if (MessageInfo.MSG_TYPE_CUSTOM == type && data.trim().length() > 0) {
+            PushUtil.getInstance().PushNotify(data, info.getDesc());
         } else {
             PushUtil.getInstance().PushNotify(info.getNickName(), info.getExtra() == null ? "" : info.getExtra().toString());
         }
